a90680e9583cefa945e4965656c2e57c4546eb92
Ramdas <mramdas@gmail.com>
2014-09-07 18:24:32 +0530

    adding sequencer

--- course_examples/simple_sequencer.svh
+++ course_examples/simple_sequencer.svh
@@ -1,4 +1,9 @@
 class simple_sequencer extends uvm_sequencer #(simple_item, simple_rsp);
+ 
+ `uvm_component_utils(simple_sequencer);
 
+  function new (input string name, uvm_component parent=null)'
+    super.new(name, parent);
+  endfunction
 
-endclass
+endclass : simple_sequencer
